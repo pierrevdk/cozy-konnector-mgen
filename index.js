166934948e18950fac721e94d7afd975c2c6644a
Travis CI User <travis@example.org>
2017-11-24 16:33:03 +0000

    publish: fix: remove duplicates from subtype
    generated from commit fc18179d96082954e3600cb16a31fb6717d55a2d

--- index.js
+++ index.js
@@ -54267,6 +54267,8 @@ connector.fetchDetailsRemboursement = function (entry, action, formData) {
       return memo
     }, {designation: [], remboursementSS: 0, remboursementMGEN: 0})
     entry.amount = round(sums.remboursementMGEN)
+    // remove duplicates
+    sums.designation = Array.from(new Set(sums.designation))
     entry.subtype = sums.designation.join(', ')
     entry.socialSecurityRefund = round(sums.remboursementSS)
 
